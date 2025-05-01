ef5665135bcfccc2c770e3508d6f5faf4c71657c
Arnomasti <arnomasti@gmail.com>
2025-05-01 19:25:44 +0300

    First commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,16 +1,34 @@
-#я честно пыталась сделать без менеджера, но он мне выдавал ошибку на драйвер, хотя путь вроде верный указывала... нашла, что можно так)
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.saucedemo.com")
 
-username = driver.find_element(By.ID, "user-name")
-password = driver.find_element(By.ID, "password")
-button = driver.find_element(By.ID, "login-button")
 
-print("Элементы найдены!")
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
 
-driver.quit()
+
+username = driver.find_element(By.ID, 'user-name')
+
+if username is None:
+    print('Элементы не найдены')
+else:
+    print('Элемент найден')
+
+
+password = driver. find_element(By. ID, 'password')
+
+if password is None:
+    print('Элементы не найдены')
+else:
+    print('Элемент найден')
+
+
+submit = driver.find_element(By.ID, 'login-button')
+
+if submit is None:
+    print('Элементы не найдены')
+else:
+    print('Элемент найден')
+
+#спасибо за хорошую проверку! Переделала! Я час возилась не понимала, почему делая все строго по уроку все равно не находит элемент!
+#А оказывается все дело во внимательности... ахахха, одна деталь не так и ничего уже не работает! Но так смешно, когда ее находишь спустя столько потраченного времени!:)
